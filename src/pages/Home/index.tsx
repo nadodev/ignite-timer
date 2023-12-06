762785887a3bb67047f05f6e5ee84cac9390a7c1
leonardo <bemmenos@gmail.com>
2023-12-06 11:54:40 -0300

    chore: adicionado estilo home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,32 @@
+import { Play } from 'phosphor-react'
+import { CountdownContainer, FormContainer, HomeContainer } from './style'
+
 export function Home() {
-  return <div>Home</div>
+  return (
+    <HomeContainer>
+      <form>
+        <FormContainer>
+          <label htmlFor="tasnk">Vou trabalhar em</label>
+          <input type="text" id="task" />
+
+          <label htmlFor="tasnk">durante</label>
+          <input type="number" id="minutesAmmount" />
+
+          <span>minutes.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <span>:</span>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <button type="submit">
+          <Play /> Começar
+        </button>
+      </form>
+    </HomeContainer>
+  )
 }
