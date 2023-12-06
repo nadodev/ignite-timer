1cea460b4d8ba84583c2a2bd429cbb29833f67d7
leonardo <bemmenos@gmail.com>
2023-12-05 21:08:40 -0300

    chore: adicionado tipos styled-components

---
+++ src/@types/ButtonVariant.ts
@@ -0,0 +1,1 @@
+export type ButtonVariants = 'red' | 'blue' | 'green' | 'yellow'
---
+++ src/components/Button.tsx
@@ -0,0 +1,14 @@
+import { ButtonVariants } from '../@types/ButtonVariant'
+import { ButtonContainer } from './styles'
+
+
+
+interface ButtonProps {
+    color: ButtonVariants
+}
+
+export  function Button({ color }: ButtonProps) {
+  return (
+    <ButtonContainer variant={color}>Button</ButtonContainer>
+  )
+}
