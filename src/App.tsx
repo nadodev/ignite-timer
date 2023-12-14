b67541e09d03dc78f9642cda232c4badd250b20d
leonardo <bemmenos@gmail.com>
2023-12-14 15:16:56 -0300

    ajustado de next para react a insight

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/themeDefault'
 import { GlobalStyle } from './styles/global'
 import { ActivityCycleProvider } from './context/CycleContext'
-import { SpeedInsights } from '@vercel/speed-insights/next'
 
 export function App() {
   return (
@@ -13,7 +12,6 @@ export function App() {
         <GlobalStyle />
         <ActivityCycleProvider>
           <Router />
-          <SpeedInsights />
         </ActivityCycleProvider>
       </ThemeProvider>
     </BrowserRouter>
